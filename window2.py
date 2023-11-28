f76d43b2a846396b63ca718d4df89c699580b3ae
Anonymus398 <71285836+Anonymus398@users.noreply.github.com>
2023-11-28 21:40:05 +0300

    123

--- window2.py
+++ window2.py
@@ -2,15 +2,14 @@ import flet as ft
 import subprocess
 import hashlib
 
-"""
-Create new user
+#Create new user
 def new_usr(pasword):
     salt = "51812910"
-    pas = str(password) + salt 
+    pas = str(pasword) + salt 
     pas_bytes = pas.encode('utf-8')
     hesh = hashlib.sha512(pas_bytes)
     return hesh.hexdigest()
-"""
+
 
 def get_log(password):
     salt = "518129"
@@ -31,6 +30,22 @@ def main(page: ft.Page):
         page.window_height = 400  
         page.window_resizable = False  
         page.update()
+        
+        def reg_button(e):
+            log = login_txt.value
+            pas = new_usr(password.value)
+            page.remove(button3)
+            page.add(button1, button2)
+            login_txt.value = ""
+            password.value = ""
+            page.update()
+            password.focus()
+            print(log)
+            print(pas)
+            
+
+
+
         def btn_click(e):
             if str(login_txt.value) == User and get_log(password) == True:
                 page.window_destroy()
@@ -39,11 +54,23 @@ def main(page: ft.Page):
             password.value = ""
             page.update()
             password.focus()
+
+        def on_text_button_click(e):
+            page.remove(button1, button2)
+            page.add(button3)
+            #page.remove_all() 
+
+        button1 = ft.ElevatedButton("Войти", on_click=btn_click)
+        button2 = ft.TextButton("Регистрация", on_click=on_text_button_click)
+        button3 = ft.ElevatedButton("Регистрация", on_click=reg_button)
         page.add(login_txt, 
                  password, 
-                 ft.ElevatedButton("Войти", on_click=btn_click), 
+                 button1,
+                 button2, 
                  greetings,
+                 
                  )
+        
         
 if __name__ == "__main__":
     ft.app(target=main)
